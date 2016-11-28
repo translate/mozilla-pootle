b2004b47c29e6ad8989788f0bceca78a54d828d6
Ryan Northey <ryan@synca.io>
2016-11-28 12:41:21 +0000

    Add l20n unit_display provider

--- mozilla_pootle/providers.py
+++ mozilla_pootle/providers.py
@@ -8,12 +8,22 @@
 
 from collections import OrderedDict
 
-from pootle.core.delegate import format_registration
+from pootle.core.delegate import format_registration, unit_display
 from pootle.core.plugin import provider
+from pootle_store.models import Unit
 
+from .display import L20nUnitDisplay
 from .formats import MOZILLA_FORMATS
+
+
+l20n_unit_display = L20nUnitDisplay()
 
 
 @provider(format_registration)
 def register_formats(**kwargs_):
     return OrderedDict(MOZILLA_FORMATS)
+
+
+@provider(unit_display, sender=Unit)
+def l20n_unit_display_provider(**kwargs_):
+    return dict(l20n=l20n_unit_display)
