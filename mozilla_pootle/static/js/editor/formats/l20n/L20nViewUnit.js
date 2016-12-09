658d036ebb471e4a2382a3cc83d686c8ad9c2564
Taras Semenenko <taras.semenenko@gmail.com>
2016-12-10 02:18:25 +0300

    Adjust L20nViewUnit to use variantLabel component

--- mozilla_pootle/static/js/editor/formats/l20n/L20nViewUnit.js
+++ mozilla_pootle/static/js/editor/formats/l20n/L20nViewUnit.js
@@ -27,14 +27,6 @@ const L20nViewUnit = React.createClass({
     };
   },
 
-  getPluralFormName(index) {
-    if (this.l20nUnit.state.getShortPluralFormName) {
-      return this.l20nUnit.state.getShortPluralFormName(index);
-    }
-
-    return `[${index}]`;
-  },
-
   componentWillMount() {
     this.l20nUnit = new L20nUnit(this.props.values[0]);
     if (this.l20nUnit.state !== null) {
@@ -47,13 +39,17 @@ const L20nViewUnit = React.createClass({
     if (this.state.isRichModeEnabled) {
       extraProps.innerComponent = InnerPre;
     }
+    if (this.l20nUnit.state !== null) {
+      extraProps.labelComponent = this.l20nUnit.state.getVariantLabelComponent();
+      extraProps.getLabel = this.l20nUnit.state.ctx ?
+        (i) => this.l20nUnit.state.ctx.getShortTitle(i) : null;
+    }
 
     return (
       <ViewUnit
         {...this.props}
         {...this.state }
         {...extraProps}
-        getPluralFormName={this.getPluralFormName}
       />
     );
   },
